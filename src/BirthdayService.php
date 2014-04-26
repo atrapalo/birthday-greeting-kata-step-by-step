a1e149b135baa37fb06aafcf0af1ba1374479960
Christian Soronellas <christian.soronellas@atrapalo.com>
2014-04-26 11:53:22 +0200

    Extract Class del método que recoge empleados

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -44,19 +44,7 @@ class BirthdayService
 
     private function findEmployeesWhoseBirthdayIs(XDate $xDate, $fileName)
     {
-        $fileHandler = fopen($fileName, 'r');
-        fgetcsv($fileHandler);
-
-        $employees = [];
-        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
-            $employeeData = array_map('trim', $employeeData);
-            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-
-            if ($employee->isBirthday($xDate)) {
-                $employees[] = $employee;
-            }
-        }
-
-        return $employees;
+        $repository = new FileEmployeeRepository();
+        return $repository->findEmployeesWhoseBirthdayIs($xDate, $fileName);
     }
 }
